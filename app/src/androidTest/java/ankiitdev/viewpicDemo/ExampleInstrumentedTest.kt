8677f0a6e7e9a74aaf1b045d79bf3832d00f11e8
Ankit Sharma <ankiitdev@gmail.com>
2022-11-13 23:49:33 +0530

    code refactoring

--- app/src/androidTest/java/ankiitdev/viewpicDemo/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/ankiitdev/viewpicDemo/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package ankiitdev.viewpic
+package ankiitdev.viewpicDemo
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
---
+++ app/src/main/java/ankiitdev/viewpicDemo/MainActivity.kt
@@ -0,0 +1,11 @@
+package ankiitdev.viewpicDemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
